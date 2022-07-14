ad14eb1ea94a2d0af93fc97462a6642e0e788f77
CallmeAmine <aminemi00@gmail.com>
2022-07-14 11:00:26 +0100

    Commit

--- lib/src/view/expense_tracker.dart
+++ lib/src/view/expense_tracker.dart
@@ -4,6 +4,8 @@ import 'package:expense_tracker/src/widget/transaction_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
 
 class ExpenseTracker extends StatefulWidget {
   const ExpenseTracker({Key? key}) : super(key: key);
@@ -14,7 +16,6 @@ class ExpenseTracker extends StatefulWidget {
 }
 
 class _ExpenseTrackerState extends State<ExpenseTracker> {
-
   @override
   void dispose() {
     Hive.close();
@@ -23,10 +24,26 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
 
   @override
   Widget build(BuildContext context) {
+    var now = new DateTime.now();
+    var formatter = new DateFormat('dd');
+    String formattedDate = formatter.format(now);
+    int daysInMonth =
+        DateTimeRange(start: now, end: DateTime(now.year, now.month + 1))
+            .duration
+            .inDays;
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Expense Tracker'),
+          title: Text(
+              'Today is Day ' +
+                  formattedDate +
+                  ' , ' +
+                  daysInMonth.toString() +
+                  ' days left ',
+              style: TextStyle(color: Colors.black)),
           centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
         body: ValueListenableBuilder<Box<Transaction>>(
           valueListenable: Boxes.getTransaction().listenable(),
@@ -36,13 +53,13 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
           },
         ),
         floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-          showDialog(
-              context: context,
-              builder: (context) =>
-                  TransactionDialog(onClickDone: addTransaction));
-        }));
+            child: const Icon(Icons.add),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) =>
+                      TransactionDialog(onClickDone: addTransaction));
+            }));
   }
 
   Widget buildContent(List<Transaction> transactions) {
@@ -59,7 +76,7 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
           (previousValue, transaction) => transaction.isExpense
               ? previousValue - transaction.amount
               : previousValue + transaction.amount);
-      final newExpenseString = 'Rs. ${netExpense.toStringAsFixed(2)}';
+      final newExpenseString = ' ${netExpense.toStringAsFixed(2)} DZD ';
       final color = netExpense > 0 ? Colors.green : Colors.red;
 
       return Column(
@@ -67,22 +84,50 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
           const SizedBox(
             height: 24.0,
           ),
-          Text(
-            'Net Expense $newExpenseString',
-            style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: 20, color: color),
+          Card(
+            shape: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(40),
+                borderSide: const BorderSide(color: Colors.white)),
+            elevation: 4,
+            color: Colors.indigo,
+            child: SizedBox(
+              height: 70,
+              width: 300,
+              child: Center(
+                child: Text(
+                  '$newExpenseString',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.white),
+                ),
+              ),
+            ),
           ),
           const SizedBox(
             height: 24.0,
           ),
           Expanded(
-              child: ListView.builder(
-                  padding: const EdgeInsets.all(8),
-                  itemCount: transactions.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final transaction = transactions[index];
-                    return buildTransaction(context, transaction);
-                  }))
+            child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: transactions.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final transaction = transactions[index];
+                  return buildTransaction(context, transaction);
+                }),
+          ),/*
+    Stack(
+    children: const <Widget>[
+    CircularProgressIndicator(
+    value: 0.7,
+    valueColor: AlwaysStoppedAnimation<Color>(Colors.purple),
+    ),
+    CircularProgressIndicator(
+    value: 0.6,
+    valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+    ),
+    ]
+    )*/
         ],
       );
     }
@@ -91,7 +136,7 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
   Widget buildTransaction(BuildContext context, Transaction transaction) {
     final color = transaction.isExpense ? Colors.red : Colors.green;
     final date = DateFormat.yMMMd().format(transaction.createdAt);
-    final amount = 'Rs. ' + transaction.amount.toStringAsFixed(2);
+    final amount = 'DZD. ' + transaction.amount.toStringAsFixed(2);
 
     return Card(
       color: Colors.white,
@@ -118,23 +163,22 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
   Widget buildButtons(BuildContext context, Transaction transaction) => Row(
         children: [
           Expanded(
-              child: TextButton.icon(
-                  onPressed: () => Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => TransactionDialog(
-                      transaction: transaction,
-                      onClickDone: (name, amount, isExpense ) => 
-                      editTransaction(transaction, name, amount, isExpense)))), 
-                  icon: const Icon(Icons.edit), 
-                  label: const Text('Edit'), 
-          ),
-        ),
-          Expanded(
             child: TextButton.icon(
-              label: const Text('Delete'),
-              icon: const Icon(Icons.delete), 
-              onPressed: ()=> deleteTransaction(transaction), 
-              
-              ))
+              onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => TransactionDialog(
+                      transaction: transaction,
+                      onClickDone: (name, amount, isExpense) => editTransaction(
+                          transaction, name, amount, isExpense)))),
+              icon: const Icon(Icons.edit),
+              label: const Text('Edit'),
+            ),
+          ),
+          Expanded(
+              child: TextButton.icon(
+            label: const Text('Delete'),
+            icon: const Icon(Icons.delete),
+            onPressed: () => deleteTransaction(transaction),
+          ))
         ],
       );
 
@@ -148,18 +192,16 @@ class _ExpenseTrackerState extends State<ExpenseTracker> {
     final box = Boxes.getTransaction();
     box.add(transaction);
   }
-  void editTransaction(
-    Transaction transaction, 
-    String name, 
-    double amount, 
-    bool isExpense 
-    ){
-      transaction.name = name;
-      transaction.amount = amount;
-      transaction.isExpense = isExpense;
 
-      transaction.save();
+  void editTransaction(
+      Transaction transaction, String name, double amount, bool isExpense) {
+    transaction.name = name;
+    transaction.amount = amount;
+    transaction.isExpense = isExpense;
+
+    transaction.save();
   }
+
   void deleteTransaction(Transaction transaction) {
     transaction.delete();
   }
